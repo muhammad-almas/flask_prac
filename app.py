dfbffe0e90f1d76f58864e6055f0c9af99d5023a
muhammad almas <muhammad.almas@gmail.com>
2024-12-23 01:42:42 -0600

    added render_template for index.html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+#import the Flask class and create an instance of it
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#define homepage route and index() view function
 # Pass the required route to the decorator
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
 
+
+#run the app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
