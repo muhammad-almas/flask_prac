631dd5f8473a964770595611c50a7eb50b7424a6
muhammad almas <muhammad.almas@gmail.com>
2024-12-23 02:15:17 -0600

    testing post route 1

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# test post dictionary
+posts = {
+    1: {'title': 'Introduction to Flask', 'content': 'Flask is a lightweight WSGI web application framework...'},
+    2: {'title': 'Understanding Routes in Flask', 'content': 'Routes are a fundamental concept in Flask...'}
+}
+
 #define homepage route and index() view function
 # Pass the required route to the decorator
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# testing post route
+@app.route('/post/1')
+def show_post():
+    post = posts[1]
+    return f"<h1>{post['title']}</h1><p>{post['content']}</p>"
 
 #run the app in debug mode
 if __name__ == '__main__':
